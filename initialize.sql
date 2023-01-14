de3e45601850486921fcc83e2a840f7693f2ebcf
Emilia Ivanova <75259556+emilia-m-ivanova@users.noreply.github.com>
2023-01-14 19:41:34 +0200

    added docker-compose, changed init script

--- initialize.sql
+++ initialize.sql
@@ -1,36 +1,41 @@
 CREATE SCHEMA country_app;
 USE country_app;
 CREATE TABLE users(
-`id` INT NOT NULL PRIMARY KEY, 
-`username` VARCHAR(256) NOT NULL, 
+`id` INT NOT NULL PRIMARY KEY,
+`username` VARCHAR(256) NOT NULL,
 `password` VARCHAR(256) NOT NULL,
 `role` VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE countries(
-`id` INT NOT NULL PRIMARY KEY, 
+`id` INT NOT NULL PRIMARY KEY,
 `name` VARCHAR(50) NOT NULL,
 `population` INT
 );
-INSERT INTO countries VALUES 
-('Russia',145934462),
-('Germany',83783942),
-('United Kingdom',67886011),
-('France',65273511),
-('Italy',60461826),
-('Spain',46754778),
-('Ukraine',43733762),
-('Poland',37846611),
-('Romania',19237691),
-('Netherlands',17134872),
-('Belgium',11589623),
-('Czech Republic',10708981),
-('Greece',10423054),
-('Portugal',10196709),
-('Sweden',10099265),
-('Hungary',9660351),
-('Belarus',9449323),
-('Austria',9006398),
-('Serbia',8737371),
-('Switzerland',8654622),
-('Bulgaria',6948445);
+INSERT INTO countries VALUES
+(1,'Russia',145934462),
+(2,'Germany',83783942),
+(3,'United Kingdom',67886011),
+(4,'France',65273511),
+(5,'Italy',60461826),
+(6,'Spain',46754778),
+(7,'Ukraine',43733762),
+(8,'Poland',37846611),
+(9,'Romania',19237691),
+(10,'Netherlands',17134872),
+(11,'Belgium',11589623),
+(12,'Czech Republic',10708981),
+(13,'Greece',10423054),
+(14,'Portugal',10196709),
+(15,'Sweden',10099265),
+(16,'Hungary',9660351),
+(17,'Belarus',9449323),
+(18,'Austria',9006398),
+(19,'Serbia',8737371),
+(20,'Switzerland',8654622),
+(21,'Bulgaria',6948445);
+
+CREATE TABLE users_seq(`next_val` BIGINT);
+
+INSERT INTO users_seq VALUES
+(101);
